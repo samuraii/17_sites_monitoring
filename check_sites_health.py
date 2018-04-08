e285707d30034fb301620236857b765c74df612b
samuraii <7thsamurai@mail.ru>
2018-04-09 00:26:11 +0300

    Правки по ревью
    1) Изменил проверку на истечение срока согласно заданию (миниму на 30 дней)

--- check_sites_health.py
+++ check_sites_health.py
@@ -9,13 +9,15 @@ def load_urls4check(path_to_file):
         domains = file_object.readlines()
         return [domain.strip() for domain in domains]
 
+
 def is_domain_ok(url):
     response = requests.get(url)
     return response.ok
 
 
-def get_domain_expiration_date(domain):
-    return whois.query(domain).expiration_date - datetime.datetime.today()
+def domain_is_paid_30_days_or_more(domain, days=30):
+    exp_date = whois.query(domain).expiration_date
+    return datetime.datetime.today() + datetime.timedelta(days) <= exp_date
 
 
 if __name__ == '__main__':
@@ -23,12 +25,9 @@ if __name__ == '__main__':
         file_with_url = sys.argv[1]
     except IndexError:
         exit('File not passed to script.')
-    except FileNotFoundError:
-        exit('File not found.')
-
     domains = load_urls4check(file_with_url)
     for domain in domains:
         print('Domain:', domain)
-        print('Domain is ok:', is_domain_ok(domain))
-        print('Domain expires in', get_domain_expiration_date(domain))
+        print('It is ok:', is_domain_ok(domain))
+        print('It is paid >= 30 days:', domain_is_paid_30_days_or_more(domain))
         print('-'*20)
