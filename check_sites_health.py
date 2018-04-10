19882a2445627670e9209a660156e91b3b749ca0
samuraii <7thsamurai@mail.ru>
2018-04-10 16:06:06 +0000

    Перенес загрузку данных в except. Добавил метод для вывода результатов проверки домена

--- check_sites_health.py
+++ check_sites_health.py
@@ -24,17 +24,27 @@ def is_domain_paid(domain, days=30):
         else:
             return exp_date > time_delta
     except TypeError:
-        return 'No expiration date found.'
+        return None
+
+def print_domain_check_results(domain):
+    print('Domain', domain)
+    print('Is it ok:', is_server_ok(domain))
+    is_paid = is_domain_paid(domain)
+    if is_paid == None:
+        print('No expiration date info for this domain.')
+    else:
+        print('Is it paid:', is_paid)
+    
 
 if __name__ == '__main__':
     try:
         file_with_url = sys.argv[1]
-    except (IndexError, FileNotFoundError):
+        domains = load_urls4check(file_with_url)
+    except IndexError:
         exit('File not passed to script.')
+    except FileNotFoundError:
+        exit('File not found.')
 
-    domains = load_urls4check(file_with_url)
     for domain in domains:
-        print('Domain:', domain)
-        print('Is it ok:', is_server_ok(domain))
-        print('Is it paid:', is_domain_paid(domain))
+        print_domain_check_results(domain)
         print('-'*20)
